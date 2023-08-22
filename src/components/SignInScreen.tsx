fb56a12bac3fa908d191326f178fab9533362097
artiefellype <arthurfellype21@gmail.com>
2023-08-21 23:10:38 -0300

    fix. Change flour and adjust loading

--- src/components/SignInScreen.tsx
+++ src/components/SignInScreen.tsx
@@ -4,18 +4,22 @@ import { FaGofore } from 'react-icons/fa'
 
 interface SignInScreenProps {
   loginWithGoogle: ()=>void;
+  loading: (value: boolean) => void
 }
-const SignInScreen = ({loginWithGoogle}: SignInScreenProps) => {
+const SignInScreen = ({loginWithGoogle, loading}: SignInScreenProps) => {
   return (
     <div className='w-screen h-screen  flex justify-center items-center bg-animb bg-center'>
-        <div className='bg-[rgba(17,25,40,.75)] border-[1px] border-[rgba(255,255,255,.125)] rounded-lg backdrop-filter backdrop-blur-[16px] backdrop-saturate-[180%] w-11/12 h-4/6 flex flex-col justify-around items-center relative'>
+        <div className=' w-11/12 h-screen flex flex-col justify-around items-center relative'>
           <div className='absolute flex flex-row items-center top-12 inset-x-auto'>
             <Image src={'/imgs/fivechan_logo.png'} alt={'fivechan logo'} width={70} height={70}/>
             <h1 className='text-3xl font-bold ml-2'>5chan</h1>
           </div>
-          <div className='flex flex-col gap-1 items-center'>
-            <h1>Sign In with</h1>
-            <button onClick={loginWithGoogle} className='border-red-500 border-solid border-2 w-24 h-9 flex flex-row justify-center items-center'><FaGofore className="mb-[1px]" />oogle</button>
+          <div className='flex flex-col gap-2 items-center w-full'>
+            <h1>Sign In</h1>
+            <button onClick={() => {
+              loginWithGoogle()
+              loading(true)
+              }} className='border-red-500 border-solid border-2 w-2/3 h-9 rounded-3xl flex flex-row justify-center items-center gap-2'> <FaGofore className="mb-[1px] bg-slate-200 text-gray-700 rounded-full" /> Continue com Google</button>
           </div>
         </div>
     </div>
--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,43 +1,70 @@
-import React from 'react'
+import React from "react";
 
-import { userContext } from '@/context/appContext';
-import { auth, entrarComGoogle, logout } from '@/firebase/authentication';
-import { fireApp } from '@/firebase/firebase';
-import Forum from '@/pages/forum';
-import { useRouter } from 'next/navigation';
-import { useContext, useEffect, useState } from 'react';
-import SignInScreen from '@/components/SignInScreen';
-
+import { userContext } from "@/context/appContext";
+import { auth, entrarComGoogle, logout } from "@/firebase/authentication";
+import { fireApp } from "@/firebase/firebase";
+import Forum from "@/pages/forum";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import SignInScreen from "@/components/SignInScreen";
+import Image from "next/image";
 
 const Login = () => {
+  const { user, setUser } = useContext(userContext);
+  const [isLoading, setIsloading] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
+  const router = useRouter();
 
-    const {user, setUser} = useContext(userContext);
-  
-    const [isLogged, setIsLogged] = useState(false);
-    const router = useRouter();
-    
-  
-    useEffect(() => {
-      auth.onAuthStateChanged((user)=>{
+  const setUserLogged = async () => {
+    setIsloading(true);
+    auth.onAuthStateChanged(
+      (user) => {
         setUser({
           isAuth: true,
           user: user,
-        })
+        });
 
-        if(user != null){
-           setIsLogged(true)
-        }
-        else setIsLogged(false)
-      }, (err)=> {})
-    }, [])
-  
-    
+        if (user != null) {
+          setIsLogged(true);
+        } else setIsLogged(false);
+        setIsloading(false);
+      },
+      (err) => {
+        setIsloading(false);
+      }
+    );
+  };
+
+  useEffect(() => {
+    setUserLogged();
+  }, []);
 
   return (
-    <div>
-      { isLogged ? <Forum />: <SignInScreen loginWithGoogle={entrarComGoogle} />}
-    </div>
-  )
-}
+    <>
+      {isLoading ? (
+        <div className="bg-animb bg-center w-screen h-screen flex justify-center items-center">
+          <Image
+            className="animate-bounce duration-3000 ease-in-out infinite"
+            src={"/imgs/fivechan_logo.png"}
+            alt={"Logo loading"}
+            width={100}
+            height={100}
+          />
+        </div>
+      ) : (
+        <div>
+          {isLogged ? (
+            <Forum />
+          ) : (
+            <SignInScreen
+              loginWithGoogle={entrarComGoogle}
+              loading={setIsloading}
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Login
+export default Login;
