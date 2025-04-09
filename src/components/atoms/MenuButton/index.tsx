2777f7f3b2890bbdb34ea107a493acbd86ef3d4a
artiefellype <arthurfellype21@gmail.com>
2025-04-09 15:04:38 -0300

    fix. header position

--- src/components/atoms/MenuButton/index.tsx
+++ src/components/atoms/MenuButton/index.tsx
@@ -1,39 +1,36 @@
-import React, { ButtonHTMLAttributes } from 'react'
-import Image from 'next/image'
+import React, { ButtonHTMLAttributes } from "react";
+import Image from "next/image";
 import { FaEllipsisH as Dots } from "react-icons/fa";
-import { User } from '@/context';
-import { UserProps } from '@/types';
+import { User } from "@/context";
+import { UserProps } from "@/types";
 
-export interface MenuButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+export interface MenuButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   user?: UserProps;
 }
 
-export const MenuButton = ({user, ...rest}: MenuButtonProps) => {
-
+export const MenuButton = ({ user, ...rest }: MenuButtonProps) => {
   return (
-    <button 
-      {...rest}
-      >
-        {/* FOTO DE PERFIL */}
-        
-        <div className='w-10 h-10 flex justify-center items-center'>
-        <Image 
-            src={user?.photoURL || "/imgs/default_perfil.jpg"}
-            alt={'perfil photo'} 
-            width={40} 
-            height={40}
-            className='rounded-full'
+    <button {...rest}>
+      <div className="flex flex-col justify-center items-end text-primary">
+        <h3 className="font-semibold h-5 text-base">{user?.displayName}</h3>
+        <p className="font-extralight text-sm">{user?.email}</p>
+      </div>
+      <div className="w-10 h-10 flex justify-center items-center">
+        <Image
+          src={user?.photoURL || "/imgs/default_perfil.jpg"}
+          alt={"perfil photo"}
+          width={40}
+          height={40}
+          className="rounded-full"
         />
-        </div>
-        {/* Nome E EMAIL */}
-        <div className='flex flex-col justify-center items-start text-primary'>
-          <h3 className='font-semibold h-5 text-base'>{user?.displayName}</h3>
-          <p className='font-extralight text-sm'>{user?.email}</p>
-        </div>
-        {/* Botao */}
-        <div className=' flex justify-center items-center'>
+      </div>
+
+      
+
+      {/* <div className=' flex justify-center items-center'>
           <Dots className='fill-primary' size={20}/>
-        </div>
+        </div> */}
     </button>
-  )
-}
+  );
+};
