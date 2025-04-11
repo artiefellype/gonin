f4b0c7d41fac6cadc9ea4a126a7dc8838f588596
artiefellype <arthurfellype21@gmail.com>
2025-04-11 16:17:52 -0300

    fix. change logout

--- src/components/molecules/SideOptions/index.tsx
+++ src/components/molecules/SideOptions/index.tsx
@@ -1,9 +1,11 @@
-import { Layout, Menu, MenuProps } from "antd";
+import { useUserContext } from "@/context";
+import { Divider, Layout, Menu, MenuProps } from "antd";
 import Sider from "antd/es/layout/Sider";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useMemo, useState } from "react";
 import { IconType } from "react-icons";
+import { TbLogin as LogoutIcon } from "react-icons/tb";
 
 export interface MenuItemsProps {
   label: React.ReactNode;
@@ -22,6 +24,7 @@ interface Props {
 type MenuItem = Required<MenuProps>["items"][number];
 
 export const SideOptions = ({ children, items }: Props) => {
+  const { signOut } = useUserContext();
   const [sideBarItemsm, stateSetSideBarItemsm] = useState<MenuItem[]>([]);
   const { Sider, Content } = Layout;
 
@@ -51,9 +54,7 @@ export const SideOptions = ({ children, items }: Props) => {
           color: "#374151",
           marginBottom: 10,
         },
-        icon: Icon ? (
-          <Icon size={20} className="mr-1 fill-primary" />
-        ) : null,
+        icon: Icon ? <Icon size={20} className="mr-1 fill-primary" /> : null,
         children,
         label: to ? <Link href={to}>{label}</Link> : label,
       } as MenuItem;
@@ -70,29 +71,41 @@ export const SideOptions = ({ children, items }: Props) => {
     stateSetSideBarItemsm(sideBarItems as MenuItem[]);
   }, [items]);
 
+  const handleLogout = async () => {
+    await signOut();
+    router.push("/login");
+  };
+
   return (
     <Layout className="max-w-7xl w-full">
-      <Sider 
-        trigger={null} 
-        width={250}
-        style={{ }}
-        >
+      <Sider trigger={null} width={250} style={{}}>
         <div className="h-full w-full bg-background pt-10">
           <Menu
             mode="inline"
             defaultSelectedKeys={[activePath]}
             defaultOpenKeys={[activePath]}
             className=" border-none rounded-lg"
-            style={{backgroundColor: '#D6D6D6', borderRight: 0, borderLeft: 0}}
+            style={{
+              backgroundColor: "#D6D6D6",
+              borderRight: 0,
+              borderLeft: 0,
+            }}
             items={sideBarItemsm}
           />
+          <Divider />
+          <button
+            onClick={handleLogout}
+            className="hover:bg-gray-100 transition-all delay-75 cursor-pointer w-[250px] h-10 rounded-md flex justify-start px-6 py-4 items-center "
+          >
+            <LogoutIcon size={24} className="mr-2 " color="#374151" />
+            <p className="text-[#374151] text-base">SAIR</p>
+          </button>
         </div>
-        
       </Sider>
       <Layout>
         <Content
           className={`min-h-screen relative flex justify-center items-start flex-row gap-4`}
-          style={{ backgroundColor: '#D6D6D6'}}
+          style={{ backgroundColor: "#D6D6D6" }}
         >
           {children}
         </Content>
